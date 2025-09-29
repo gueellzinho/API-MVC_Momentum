6f23f8b12b78b2a85eb6c99454ba2212e45b5c3e
gueellzinho <miguelAmorim_@outlook.com>
2025-09-29 08:59:42 -0300

    adicionei cors

--- src/config/express.js
+++ src/config/express.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const aplicacao = express();
 
 const bodyParser = require("body-parser");
 const { query, validationResult  } = require("express-validator");
 
+aplicacao.use(cors())
 aplicacao.use(bodyParser.json());
 aplicacao.use(query());
 aplicacao.use(bodyParser.urlencoded({ extended: true }));
